931112b08252a383652d96e741e560d4dc2b4142
root <root@diego.diego>
2019-11-16 00:43:59 +0100

    small fix

--- Client.cpp
+++ Client.cpp
@@ -14,26 +14,26 @@ bool areCoprime(unsigned long long int x, unsigned long long int y);
 
 int main(int argc, char const *argv[]) { 
 
-	int sock = 0; 
-	struct sockaddr_in serv_addr; 
+    int sock = 0; 
+    struct sockaddr_in serv_addr; 
 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
-		printf("\n Socket creation error \n"); 
-		return -1; 
-	} 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
 
-	serv_addr.sin_family = AF_INET; 
-	serv_addr.sin_port = htons(PORT); 
-	
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) { 
-		printf("\nInvalid address/ Address not supported \n"); 
-		return -1; 
-	} 
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
 
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
-		printf("\nConnection Failed \n"); 
-		return -1; 
-	}
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    }
     
     unsigned long long int p,q,n,pn,e;
 
--- Server.cpp
+++ Server.cpp
@@ -16,24 +16,26 @@ int main(int argc, char const *argv[]) {
     struct sockaddr_in address; 
     int opt = 1; 
     int addrlen = sizeof(address); 
-       
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
         perror("socket failed"); 
         exit(EXIT_FAILURE); 
     } 
-       
+
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
         perror("setsockopt"); 
         exit(EXIT_FAILURE); 
-    } 
+    }
+
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 
-       
+
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) { 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
     } 
+
     if (listen(server_fd, 3) < 0) { 
         perror("listen"); 
         exit(EXIT_FAILURE); 
